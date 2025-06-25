97f99a7382c60a9a17feaf154ca3b479b07b19a1
Zyared <artemii-churaev@mail.ru>
2025-06-25 10:34:43 +0300

    Init commit
    Создание начальных файлов проекта

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education_PR_2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
